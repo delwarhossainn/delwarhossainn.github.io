1bacbfdd8cd24e158ffc2526cfa86791774e5b6b
Delwar Hossain <delwar.contact@gmail.com>
2024-11-20 02:19:19 -0600

    updated web page

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const sections = document.querySelectorAll('.animated-section');
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.style.animation = 'fadeIn 1.5s forwards';
-        }
-      });
-    }, { threshold: 0.2 });
-  
-    sections.forEach((section) => observer.observe(section));
-  });
-  
+  const navbar = document.querySelector('.navbar');
+  const heroSection = document.querySelector('.hero-section');
+
+  const observer = new IntersectionObserver(
+    ([entry]) => {
+      if (!entry.isIntersecting) {
+        navbar.classList.add('scrolled');
+      } else {
+        navbar.classList.remove('scrolled');
+      }
+    },
+    { threshold: 0.1 }
+  );
+
+  observer.observe(heroSection);
+});
